bd036b811df1d500e8083c4e8bb7709d6b3f36ac
Eswar2000 <v.eswarraman2000@gmail.com>
2022-09-19 18:35:48 +0530

    Login Static Screen Developed

--- notemaker/src/App.js
+++ notemaker/src/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './screen/login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <form action="../../temp" method="POST">
           <button>Submit</button>
         </form>
-      </header>
+      </header> */}
+      <Login />
     </div>
   );
 }
---
+++ notemaker/src/screen/login.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import FaceIcon from '@mui/icons-material/Face';
+import {blue} from '@mui/material/colors';
+
+import {Avatar, Box, Button, Container, Link, TextField, Typography} from '@mui/material';
+
+function Login() {
+  return (
+    <Container className='form-card' maxWidth='xs'>
+        <Avatar className='avatars' sx={{ bgcolor: blue[700] }}>
+            <FaceIcon fontSize='large' />
+        </Avatar >
+        <Box height={4} />
+        <Typography component="h1" variant="h5">
+        Sign In
+        </Typography>
+        <Box height={24}/>
+        <Box component="form">
+        <TextField size="small" id="email" label="Email Address" name="email" required fullWidth />
+        <Box height={24}/>
+        <TextField size="small" id="password" name="password" label="Password" type="password" required fullWidth />
+        <Box height={24}/>
+        <Button type="submit" variant="contained" fullWidth>
+            Sign In
+        </Button>
+        <Box height={4}/>
+        <Link href="#" variant="body2">
+            Forgot password?
+        </Link>
+        <Box height={4}/>
+        <Link href="#" variant="body2">
+            {"Don't have an account? Sign Up"}
+        </Link>
+        </Box>
+      </Container>
+  );
+}
+
+export default Login;
