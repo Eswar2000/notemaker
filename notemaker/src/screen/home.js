025bac42e674f37226d96660910f5f5bd62956a2
Eswar2000 <v.eswarraman2000@gmail.com>
2022-10-05 17:40:30 +0530

    Updated Dashboard Sidepanel

--- notemaker/src/screen/home.js
+++ notemaker/src/screen/home.js
@@ -1,13 +1,50 @@
-// import {useState} from 'react';
-// import {Avatar, Box, Button, Container, Typography} from '@mui/material';
-
+import NoteBanner from '../notes_banner.png';
+import {Avatar, Divider, Grid, Box, Paper, List, ListItem, ListItemAvatar, ListItemText, Typography} from '@mui/material';
+import {NoteAdd, Edit, PushPin} from '@mui/icons-material';
 import Appbar from '../components/Appbar';
 
 function home() {
     return (
         <div className='dashboard'>
             <Appbar />
-            <h1>Dashboard Here</h1>
+            <Box height={8} />
+            <Grid container spacing={4}>
+                <Grid item xs={9}>
+                    <h1>Dashboard Here</h1>
+                </Grid>
+                <Grid item xs={3}>
+                    <Paper className='side-panel' elevation={6}>
+                        <img src={NoteBanner} id='home-banner' alt='NoteMaker Home Banner'/>
+                        <Divider />
+                        <List>
+                            <ListItem>
+                                <ListItemAvatar>
+                                    <Avatar>
+                                        <NoteAdd fontSize='medium' />
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText primary='Add Notes'/>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemAvatar>
+                                    <Avatar>
+                                        <Edit fontSize='medium' />
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText primary='Edit Notes'/>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemAvatar>
+                                    <Avatar>
+                                        <PushPin fontSize='medium' />
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText primary='Pin Notes'/>
+                            </ListItem>
+                        </List>
+                    </Paper>
+                </Grid>
+            </Grid>
         </div>
     );
 }
